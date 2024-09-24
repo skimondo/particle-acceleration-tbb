895a52b8a0282ef1c81ba008a4c3d2e71732fe08
Dominique Elias <elias.dominique@courrier.uqam.ca>
2024-09-24 12:19:15 -0400

    First try to parallelise potential

--- src/potentialparallel.cpp
+++ src/potentialparallel.cpp
@@ -1,19 +1,24 @@
 #include "potentialparallel.h"
 
+#include <tbb/global_control.h>
+#include <tbb/parallel_for.h>
+
 /*
- * IDENTIFICATION : indiquer le ou les codes permanents des auteurs ici.
+ * Dominique Elias
+ * ELID14019800
+ * 24/09/2024
  *
- *
+ * ATTENTION, TO BE OPTIMIZED!!
  */
 
 void PotentialParallel::compute_field(std::vector<Particle> &charge, double &lo,
                                       double &hi) {
-  // À COMPLETER
+  
   lo = std::numeric_limits<double>::max();
   hi = std::numeric_limits<double>::min();
   int n = charge.size();
-  for (int i = 0; i < m_height; i++) {
-    for (int j = 0; j < m_width; j++) {
+  tbb::parallel_for(0, m_height, [&](int i) {
+    tbb::parallel_for(0, m_width, [&](int j) {
       double x = 1.0 * j / m_width;
       double y = 1.0 * i / m_height;
 
@@ -24,19 +29,19 @@ void PotentialParallel::compute_field(std::vector<Particle> &charge, double &lo,
       m_sol[IDX2(i, j, m_width)] = v;
       lo = std::min(lo, v);
       hi = std::max(hi, v);
-    }
-  }
+    });
+  });
 }
 
 void PotentialParallel::move_particles(std::vector<Particle> &charge, double dt,
                                        int substeps) {
-  // À COMPLETER
+  
   int n = charge.size();
   double ssdt = dt / substeps;
-  for (int ss = 0; ss < substeps; ss++) {
+  tbb::parallel_for(0, substeps, [&](int ss){
 
     // Calculer les forces entre les charges
-    for (int i = 0; i < n; i++) {
+    tbb::parallel_for(0, n, [&](int i) {
       Particle &c = charge[i];
       Vector2d f = Vector2d::Zero();
       for (int j = 0; j < n; j++) {
@@ -45,10 +50,10 @@ void PotentialParallel::move_particles(std::vector<Particle> &charge, double dt,
         }
       }
       c.m_f = f;
-    }
+    });
 
     // On déplace ensuite les charges
-    for (int i = 0; i < n; i++) {
+    tbb::parallel_for(0, n, [&](int i) {
 
       Particle &c = charge[i];
       // mettre à jour la vitesse en fonction de l'accélération
@@ -59,19 +64,19 @@ void PotentialParallel::move_particles(std::vector<Particle> &charge, double dt,
 
       // déplacer la particule en fonction de sa vitesse
       c.m_x += c.m_v * ssdt;
-    }
-  }
+    });
+  });
 }
 
 void PotentialParallel::save_solution(std::ostream &ofs, ColorMap &cmap) {
-  // À COMPLETER
-  for (int i = 0; i < m_height; i++) {
-    for (int j = 0; j < m_width; j++) {
+  
+  tbb::parallel_for(0, m_height, [&](int i) {
+    tbb::parallel_for(0, m_width, [&](int j) {
       double v = m_sol[IDX2(i, j, m_width)];
       png::rgb_pixel pix = cmap.get_color(v);
       // inversion axe y pour correspondre au plan cartésien
       m_img.set_pixel(j, m_height - i - 1, pix);
-    }
-  }
+    });
+  });
   m_img.write_stream(ofs);
 }
